6cc842b880f7de836a1352f57db0edc84929f1ca
lregebro <devnull@localhost>
2005-07-12 17:27:36 +0000

    Moved this monkeypatch to CPSonFive

--- __init__.py
+++ __init__.py
@@ -21,3 +21,39 @@
 
 This product is the container for any reusable integration between CPS and Five.
 """
+
+from zope.schema.interfaces import ITitledTokenizedTerm
+
+# Zope3.0.0 doesn't translates dropdownboxes (that's a bug)
+def textForValue(self, term):
+    """Extract a string from the term.
+
+    The term must be a vocabulary tokenized term.
+
+    This can be overridden to support more complex term objects. The token
+    is returned here since it's the only thing known to be a string, or
+    str()able."""
+    # XXX: This is how it should be once we start using Five 1.1, with i18n.
+    # if ITitledTokenizedTerm.providedBy(term):
+    #     return self.translate(term.title)
+    # return self.translate(term.token)
+
+    # XXX: But with Five 1.0.x we need to call Localizer:
+    if ITitledTokenizedTerm.providedBy(term):
+        return self.context.context.Localizer.default(term.title)
+    return self.context.context.Localizer.default(term.token)
+
+
+def initialize(context):
+
+    # Zope3monkey
+    from zope.app.form.browser.itemswidgets import ItemsWidgetBase
+    ItemsWidgetBase.textForValue = textForValue
+    
+    ## Five monkey
+    # Hm? Why? Probably this fix has been pushed into Five by now, because 
+    # this edit.pt is exactly the same as Fives...
+    #from Products.Five.browser import EditView
+    #from Products.Five.pagetemplatefile import FivePageTemplateFile
+    #EditView.generated_form = FivePageTemplateFile('edit.pt')
+    
